0e069386ee094b6f5d655bc3796dd21e84df2367
ggfcsimoes1 <flashrulez7@gmail.com>
2022-12-16 22:57:11 +0000

    :lipstick: Reduce video player size

--- src/pages/save-as-i-save/save-as-i-save.tsx
+++ src/pages/save-as-i-save/save-as-i-save.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ImageHeader from '../../components/image-header/image-header';
 import Video from '../../components/video-player/video';
 import Hint from '../../components/hint/hint';
+import Form from '../../components/form/form';
 
 interface SaveAsISaveProps {
   imgPath: string;
@@ -14,8 +15,9 @@ function SaveAsISave(props: SaveAsISaveProps) {
   return (
     <div>
       <ImageHeader path={props.imgPath} />
-      <Video path={props.vidPath} width="800px" height="500px" />
+      <Video path={props.vidPath} width="700px" height="400px" />
       <Hint path="https://media.discordapp.net/attachments/825881406017241178/1052735595601997924/hint.png" />
+      <Form />
     </div>
   );
 }
