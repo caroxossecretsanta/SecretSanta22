486c46f7b3369a94aae393db497d48f71a9c3484
ggfcsimoes1 <flashrulez7@gmail.com>
2022-12-17 23:17:32 +0000

    :sparkles: Add camera permission to 2nd puzzle

--- src/components/video-player/video.tsx
+++ src/components/video-player/video.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactPlayer from 'react-player';
 import './video.css';
 
@@ -6,9 +5,21 @@ interface VideoProps {
   path: string;
   width: string;
   height: string;
+  click?: any;
 }
 
 function Video(props: VideoProps) {
+  function getVideo() {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: { width: 1280 },
+      })
+      .catch((err) => {
+        // always check for errors at the end.
+        console.error(`${err.name}: ${err.message}`);
+      });
+  }
+
   return (
     <div className="Video">
       <div className="player-wrapper">
@@ -18,6 +29,11 @@ function Video(props: VideoProps) {
           width={props.width}
           height={props.height}
           controls={true}
+          onProgress={(e) => {
+            if (props.click && e.playedSeconds > 4) {
+              getVideo();
+            }
+          }}
         />
       </div>
     </div>
