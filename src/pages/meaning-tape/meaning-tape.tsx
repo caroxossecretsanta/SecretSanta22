49cee86ffb5771c2ea8187bb98a2f8f249db2e50
ggfcsimoes1 <flashrulez7@gmail.com>
2022-12-17 14:09:24 +0000

    :construction: Add second puzzle page files

---
+++ src/pages/meaning-tape/meaning-tape.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ImageHeader from '../../components/image-header/image-header';
+import Video from '../../components/video-player/video';
+import Hint from '../../components/hint/hint';
+import Form from '../../components/form/form';
+
+interface MeaningTapeProps {
+  imgPath: string;
+  vidPath: string;
+  width: string;
+  height: string;
+}
+
+function MeaningTape(props: MeaningTapeProps) {
+  return (
+    <div>
+      <ImageHeader path={props.imgPath} />
+      <Video path={props.vidPath} width="700px" height="400px" />
+      <Hint
+        imagePath="https://media.discordapp.net/attachments/825881406017241178/1052735595601997924/hint.png"
+        hintText="Save Image As Google B64"
+      />
+      <Form answer="placeholder" />
+    </div>
+  );
+}
+
+export default MeaningTape;
