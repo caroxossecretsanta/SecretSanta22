27ad626c3531751dcd87071fae832442d7825304
ggfcsimoes1 <flashrulez7@gmail.com>
2022-12-17 23:17:59 +0000

    :sparkles: Implement camera feature in page

--- src/pages/meaning-tape/meaning-tape.tsx
+++ src/pages/meaning-tape/meaning-tape.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ImageHeader from '../../components/image-header/image-header';
 import Video from '../../components/video-player/video';
 import Hint from '../../components/hint/hint';
@@ -15,7 +14,7 @@ function MeaningTape(props: MeaningTapeProps) {
   return (
     <div>
       <ImageHeader path={props.imgPath} />
-      <Video path={props.vidPath} width="700px" height="400px" />
+      <Video path={props.vidPath} width="700px" height="400px" click={true} />
       <Hint
         imagePath="https://media.discordapp.net/attachments/825881406017241178/1052735595601997924/hint.png"
         hintText="https://bit.ly/3hA3tas"
