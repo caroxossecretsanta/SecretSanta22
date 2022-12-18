417a8b3da4b8405c8f659db560e90539030a7343
ggfcsimoes1 <flashrulez7@gmail.com>
2022-12-18 01:47:58 +0000

    :recycle: Refactor routes

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import SaveAsISave from './pages/save-as-i-save/save-as-i-save';
 import MeaningTape from './pages/meaning-tape/meaning-tape';
+import FinalPiece from './pages/final-piece/final-piece';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -28,12 +29,22 @@ const SecondPuzzle = (
   />
 );
 
+const ThirdPuzzle = (
+  <FinalPiece
+    imgPath="https://media.discordapp.net/attachments/825881406017241178/1053844182172696596/fef7dbd5d379c401aaad75657115d56d.png"
+    vidPath="https://streamable.com/sy49ec"
+    width="800px"
+    height="500px"
+  />
+);
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="SecretSanta22" element={FirstPuzzle} />
-        <Route path="SecretSanta22/2" element={SecondPuzzle} />
+        <Route path="SecretSanta22/secondtrial" element={SecondPuzzle} />
+        <Route path="SecretSanta22/finalpuzzle" element={ThirdPuzzle} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
