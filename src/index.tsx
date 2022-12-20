d991fbe44d20c063e574696feb382896897a6562
ggfcsimoes1 <flashrulez7@gmail.com>
2022-12-20 17:12:59 +0000

    :sparkles: Add prelude teaser page

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,16 @@ import MeaningTape from './pages/meaning-tape/meaning-tape';
 import FinalPiece from './pages/final-piece/final-piece';
 import CheaterDetected from './components/cheater-detected/cheater-detected';
 import FinalPage from './pages/final-page/final-page';
+import HomePage from './pages/home-page/home-page';
+import { isMobile } from 'react-device-detect';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
-const FirstPuzzle = (
+const Prelude = <HomePage />;
+
+const FirstPuzzle = !isMobile ? (
   <SaveAsISave
     imgPath="https://media.discordapp.net/attachments/825881406017241178/1052735396150247504/c2VlIGFzIGkgc2Vl.png"
     vidPath="https://streamable.com/0vicre"
@@ -21,9 +25,11 @@ const FirstPuzzle = (
     height="500px"
     clue="Why is six afraid of seven?"
   />
+) : (
+  <p className="paragraph-text">Please visit this website on your pc :)</p>
 );
 
-const SecondPuzzle = (
+const SecondPuzzle = !isMobile ? (
   <MeaningTape
     imgPath="https://media.discordapp.net/attachments/825881406017241178/1053673322900103279/102706.png"
     vidPath="https://streamable.com/0ug6j9"
@@ -31,9 +37,11 @@ const SecondPuzzle = (
     height="500px"
     clue="A bird in hand is worth 2 in the bush."
   />
+) : (
+  <p className="paragraph-text">Please visit this website on your pc :)</p>
 );
 
-const ThirdPuzzle = (
+const ThirdPuzzle = !isMobile ? (
   <FinalPiece
     imgPath="https://media.discordapp.net/attachments/825881406017241178/1053844182172696596/fef7dbd5d379c401aaad75657115d56d.png"
     vidPath="https://streamable.com/sy49ec"
@@ -41,20 +49,25 @@ const ThirdPuzzle = (
     height="500px"
     clue="2 birds with 1 stone"
   />
+) : (
+  <p className="paragraph-text">Please visit this website on your pc :)</p>
 );
 
-const FourthPuzzle = (
+const FourthPuzzle = !isMobile ? (
   <FinalPage imgPath="https://media.discordapp.net/attachments/825881406017241178/1054486538609774742/1672c3d1ecf312666f04f759050bd55b.png" />
+) : (
+  <p className="paragraph-text">Please visit this website on your pc :)</p>
 );
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="SecretSanta22" element={FirstPuzzle} />
-        <Route path="SecretSanta22/1" element={SecondPuzzle} />
-        <Route path="SecretSanta22/2" element={ThirdPuzzle} />
-        <Route path="SecretSanta22/3" element={FourthPuzzle} />
+        <Route path="SecretSanta22" element={Prelude} />
+        <Route path="SecretSanta22/1" element={FirstPuzzle} />
+        <Route path="SecretSanta22/2" element={SecondPuzzle} />
+        <Route path="SecretSanta22/3" element={ThirdPuzzle} />
+        <Route path="SecretSanta22/4" element={FourthPuzzle} />
         <Route
           path="SecretSanta22/cheaterdetected"
           element={<CheaterDetected />}
---
+++ src/pages/home-page/home-page.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import Countdown from 'react-countdown';
+import { useNavigate } from 'react-router-dom';
+import './home-page.css';
+
+function HomePage() {
+  const [enabled, setEnabled] = useState(false);
+  localStorage.setItem('COMPLETED_PUZZLES', '-1');
+  const navigate = useNavigate();
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    localStorage.setItem('COMPLETED_PUZZLES', '0');
+    navigate('/SecretSanta22/1');
+  };
+
+  const handleCompletion = () => {
+    setEnabled(true);
+  };
+
+  return (
+    <>
+      <div>
+        <p className="paragraph-text">Hello,</p>
+        <p className="paragraph-text">
+          Time is running out. It seems most of you haven't stepped up to
+          complete your trials. Remember: In order to help others, you must
+          first help yourself.
+        </p>
+        <p className="paragraph-text">
+          You are the piece of someone else's puzzle. You might not know, as of
+          yet, where you fit. But others will fill their own puzzles with pieces
+          of you.
+        </p>
+        <p className="paragraph-text">2 days remain. Remember the rules.</p>
+      </div>
+      <Countdown
+        className="paragraph-text"
+        date={new Date('2022-12-22T17:00:00')}
+        daysInHours
+        onComplete={handleCompletion}
+      />
+      <div className="cheater-form">
+        <form onSubmit={handleSubmit}>
+          <input type="submit" value="PROCEED" disabled={!enabled} />
+        </form>
+      </div>
+
+      <p className="paragraph-text">
+        Note: You'll need a PC to solve the puzzles!
+      </p>
+    </>
+  );
+}
+
+export default HomePage;
